b268f31ef820b1d333e337c26ea648cba5908ccd
Oleksandr Kiriukhin <oleksandr.kiriukhin@gmail.com>
2025-05-28 21:37:42 +0300

    eslint + prettier fixes

---
+++ configs/playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 10000,
+  retries: 0,
+  use: {
+    baseURL: process.env.BASE_URL,
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+    video: 'retain-on-failure',
+  },
+  reporter: [['list'], ['allure-playwright']],
+});
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,35 @@
-import eslintPluginTs from '@typescript-eslint/eslint-plugin';
-import parserTs from '@typescript-eslint/parser';
+import eslintPluginTs from "@typescript-eslint/eslint-plugin";
+import parserTs from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   {
-    files: ['**/*.ts'],
+    ignores: [
+      "node_modules",
+      "dist",
+      "*.js",
+      "*.cjs",
+      "*.mjs",
+      "*.json",
+      "reports",
+      ".idea",
+      ".husky",
+    ],
+    files: ["**/*.ts"],
     languageOptions: {
       parser: parserTs,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
       },
     },
     plugins: {
-      '@typescript-eslint': eslintPluginTs,
+      "@typescript-eslint": eslintPluginTs,
+      prettier: prettierPlugin,
     },
     rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      '@typescript-eslint/no-unused-vars': ['warn'],
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      "prettier/prettier": "error",
     },
   },
 ];
